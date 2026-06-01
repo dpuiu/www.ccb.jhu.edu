41a4c7c7cffa86713bd2a3c3434a485b4f4928fd
Daniela Puiu <dpuiu@dpuiu@jhu.edu>
2026-06-01 13:19:28 -0400

    multiple updates

--- conf.py
+++ conf.py
@@ -14,7 +14,7 @@ release = '2026'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [ "myst_parser","sphinx_design"]
+extensions = [ "myst_parser","sphinx_design","sphinx.ext.githubpages"]
 myst_enable_extensions = ["colon_fence"]
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'OLD']
@@ -28,7 +28,8 @@ html_theme = 'furo' # alabaster'
 html_static_path = ['_static']
 html_js_files = ["custom.js"]
 html_css_files = [ 'custom.css', ]
-html_theme_options = { 'announcement': '<div style="display:flex;justify-content:space-between;align-items:center;background:#1D4475;color:white;"><img src="/_static/ccblogotext_wbg_50.png"><img src="/_static/jhu-wse-logo-white_50.png"></div>'}
+html_theme_options = { 'announcement': '<div style="display:flex;justify-content:space-between;align-items:center;background:#1D4475;color:white;"><img src="/www.ccb.jhu.edu/_static/ccblogotext_wbg_50.png"><img src="/www.ccb.jhu.edu/_static/jhu-wse-logo-white_50.png"></div>'}
+html_extra_path = ['_static/.nojekyll']
 
 html_use_index = True
 html_search_language = "en"
