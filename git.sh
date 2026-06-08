6e8678720a707ad561b2a857550fa8c72bfb9e4c
Daniela Puiu <dpuiu@dpuiu@jhu.edu>
2026-06-08 09:57:05 -0400

    multiple updates

--- git.sh
+++ git.sh
@@ -1,7 +1,14 @@
+#git ls-remote  http://github.com/dpuiu/www.ccb.jhu.edu
+#git ls-tree -r --name-only HEAD
 #git pull
+git rm -r --cached OLD/
+git rm -r --cached People/OLD/
+
 
 make html
-rsync -av --delete _build/html/ docs/
+#rm -r docs
+#cp -r _build/html docs
+#rsync _build/html docs
 make clean
 
 
