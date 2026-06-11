ab51ec4251504c0358147bd6e79f0b571e280ddc
Daniela Puiu <dpuiu@dpuiu@jhu.edu>
2026-06-11 13:30:49 -0400

    multiple updates

--- git.sh
+++ git.sh
@@ -6,14 +6,16 @@
 #git check-ignore -v OLD
 #git check-ignore -v */OLD
 
-make html theme=furo
-make html theme=pydata
+#make html theme=furo
+#make html theme=pydata
 
 #rm -r docs
 #cp -r _build/html docs
-rsync -a _build/furo/ docs/
-rsync -a _build/pydata/ docs/pydata
-make clean
+#rsync -a _build/furo/ docs/
+#rsync -a _build/pydata/ docs/pydata
+#make clean
+
+rm -fr _build docs/
 
 git status
 git add .
