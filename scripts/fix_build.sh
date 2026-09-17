67d39c9817a1178e0204358bde83661c236c380d
Daniela Puiu <dpuiu@dpuiu@jhu.edu>
2026-09-17 16:26:27 -0400

    multiple updates

--- scripts/fix_build.sh
+++ scripts/fix_build.sh
@@ -3,6 +3,6 @@
 set -eux
 
 find _build/pydata_sphinx_theme/ -name "*.html" -exec sed -i '/fontawesome/d' {} +
-find _build/pydata_sphinx_theme/ -name "*.html" -exec sed -i 's/script src="searchindex.js"/script defer src="searchindex.js"/' {} +
+find _build/pydata_sphinx_theme/ -name "*.html" -exec sed -i 's/script src="search/script defer src="search/' {} +
 
 rm _build/pydata_sphinx_theme/_static/scripts/fontawesome.js*
